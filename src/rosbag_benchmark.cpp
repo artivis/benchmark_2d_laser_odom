3a08592a24f4e39cdf52f8f8e9ce3105d2854fcc
Jeremie Deray <jeremie.deray@pal-robotics.com>
2017-02-28 20:12:45 +0100

    wip rosbag bench

--- src/rosbag_benchmark.cpp
+++ src/rosbag_benchmark.cpp
@@ -8,10 +8,20 @@
 
 #include <pal_rosbag_reader/pal_rosbag_reader.h>
 #include <laser_odometry_core/laser_odometry.h>
+#include <laser_odometry_core/laser_odometry_utils.h>
 
 #include <benchmark_2d_laser_odom/timer.h>
 
-using LocalizedRangeScan = std::pair<sensor_msgs::LaserScan, tf::Transform>;
+std::vector<sensor_msgs::LaserScanPtr> laser_scan_msgs;
+std::vector<nav_msgs::Odometry> odometry_msgs;
+std::vector<nav_msgs::Odometry> odometry_gt_msgs;
+
+using OdometryIt = std::vector<nav_msgs::Odometry>::iterator;
+
+bool getBaseToLaser(const tf::tfMessagePtr tf_msg, tf::Transform& tf)
+{
+  return laser_odometry::utils::getTf(tf_msg, "base_link", "base_laser_link", tf);
+}
 
 int main(int argc, char **argv)
 {
@@ -61,91 +71,113 @@ int main(int argc, char **argv)
     return EXIT_FAILURE;
   }
 
-  std::vector<LocalizedRangeScan> localized_scans;
+  std::string odometry_gt_topic;
+  if(!nh.getParam("odometry_gt_topic", odometry_gt_topic))
+  {
+    ROS_ERROR("No input file specified: use param _odometry_gt_topic:=[topic]");
+    return EXIT_FAILURE;
+  }
 
   std::shared_ptr<pal::RosbagReader> rosbag_reader_ptr_;
   rosbag_reader_ptr_ = std::make_shared<pal::RosbagReader>(rosbag_file);
 
   bool read_ok = true;
 
-  bool laser_transform_init = false;
-  geometry_msgs::TransformStamped laser_transform;
+  sensor_msgs::LaserScanPtr laser   = boost::make_shared<sensor_msgs::LaserScan>();
+  nav_msgs::OdometryPtr odometry    = boost::make_shared<nav_msgs::Odometry>();
 
-  sensor_msgs::LaserScanPtr laser = boost::make_shared<sensor_msgs::LaserScan>();
-  nav_msgs::OdometryPtr odometry = boost::make_shared<nav_msgs::Odometry>();
-  geometry_msgs::TransformStampedPtr transform_msg = boost::make_shared<geometry_msgs::TransformStamped>();
-
+  // Load all scans
   do
   {
-    read_ok = rosbag_reader_ptr_->readNext(laser_scan_topic, laser/*, odometry, transform_msg*/);
+    read_ok = rosbag_reader_ptr_->readNext(laser_scan_topic, laser);
+
+    if (read_ok) laser_scan_msgs.emplace_back(laser);
+
+  } while (read_ok);
+
+  std::cout << "Loaded " << laser_scan_msgs.size() << " scans." << std::endl;
+
+  // Load all odometry
+  do
+  {
+    read_ok = rosbag_reader_ptr_->readNext(odometry_topic, odometry);
 
     /// @todo retrieve laser relative to base.
     if (read_ok)
     {
-      tf::Transform transform = tf::Transform::getIdentity();
-//      transform.setOrigin(tf::Vector3{odometry->pose.pose.position.x,
-//                                      odometry->pose.pose.position.y,
-//                                      odometry->pose.pose.position.z});
+      //    tf::Transform transform = tf::Transform::getIdentity();
+      //      transform.setOrigin(tf::Vector3{odometry->pose.pose.position.x,
+      //                                      odometry->pose.pose.position.y,
+      //                                      odometry->pose.pose.position.z});
 
-//      tf::Quaternion q;
-//      tf::quaternionMsgToTF(odometry->pose.pose.orientation, q);
-//      transform.setRotation(q);
+      //      tf::Quaternion q;
+      //      tf::quaternionMsgToTF(odometry->pose.pose.orientation, q);
+      //      transform.setRotation(q);
 
-      localized_scans.emplace_back(*laser, transform);
-
-      if (!laser_transform_init)
-      {
-        //std::cout << "laser " << laser << std::endl;
-
-        tf::Transform laser_transform_tf = tf::Transform::getIdentity();
-        tf::transformMsgToTF(transform_msg->transform, laser_transform_tf);
-
-        auto rel = transform.inverseTimes(laser_transform_tf);
-
-        tf::transformTFToMsg(rel, laser_transform.transform);
-
-        laser_transform_init = true;
-      }
+      odometry_msgs.emplace_back(*odometry);
     }
   } while (read_ok);
 
-  std::cout << "Loaded " << localized_scans.size() << " scans." << std::endl;
+  // Load all odometry groundtruth
+  do
+  {
+    read_ok = rosbag_reader_ptr_->readNext(odometry_gt_topic, odometry);
 
-  if (localized_scans.empty()) return EXIT_SUCCESS;
+    if (read_ok) odometry_gt_msgs.emplace_back(*odometry);
+
+  } while (read_ok);
+
+  std::cout << "Loaded " << odometry_gt_msgs.size() << " odom groundtruth." << std::endl;
+
+  tf::Transform base_to_laser;
+  // Retrieve tf laser to base
+  do
+  {
+    tf::tfMessagePtr tf_msg = boost::make_shared<tf::tfMessage>();
+    read_ok = rosbag_reader_ptr_->readNext("/tf", tf_msg);
+
+    if (read_ok)
+    {
+      if (getBaseToLaser(tf_msg, base_to_laser))
+      {
+        //laser_odometry::utils::print(base_to_laser, "laser_link ");
+        read_ok = false;
+      }
+    }
+
+  } while (read_ok);
+
+  if (laser_scan_msgs.empty())  return EXIT_SUCCESS;
+  if (odometry_msgs.empty())    return EXIT_SUCCESS;
+  if (odometry_gt_msgs.empty()) return EXIT_SUCCESS;
+
+  laser_odom_ptr->setLaserPose(base_to_laser);
 
   geometry_msgs::PoseArray corrected_poses, estimated_poses;
-//  corrected_poses.header.frame_id = "map";
-//  estimated_poses.header.frame_id = "map";
-
   ros::Publisher publisher_corrected_poses, publisher_estimated_poses;
-  publisher_corrected_poses = nh.advertise<geometry_msgs::PoseArray>("corrected_poses", 1);
-  publisher_estimated_poses = nh.advertise<geometry_msgs::PoseArray>("estimated_poses", 1);
+  publisher_corrected_poses = nh.advertise<geometry_msgs::PoseArray>("/benchmark/corrected_poses", 1);
+  publisher_estimated_poses = nh.advertise<geometry_msgs::PoseArray>("/benchmark/estimated_poses", 1);
 
   // Get initial pose
-  const tf::Transform origin = localized_scans.begin()->second;
+  const tf::Transform origin = tf::Transform::getIdentity();//localized_scans.begin()->second;
   const tf::Transform origin_inv = origin.inverse();
 
   //laser_odom_ptr->setOrigin(origin);
 
-  tf::Transform tf_prev; tf_prev.setIdentity();
-  tf::Transform tf_est;  tf_est.setIdentity();
+  tf::Transform tf_prev = tf::Transform::getIdentity();
+  tf::Transform tf_est  = tf::Transform::getIdentity();
 
-  //nav_msgs::Odometry odometry_w;
-  nav_msgs::Path path;
-  path.header.frame_id = "map";
-
-//  lsr::geometry::RigidTransform3s tf_rel_est = lsr::geometry::Idendity3ds;
-
-//  pal_robot_tools::TimerData timer("toto");
   pal::TimerU timer;
 
   tf::Transform corrected_pose;
 
-  tf::Transform initial_guess; initial_guess.setIdentity();
+  tf::Transform initial_guess = tf::Transform::getIdentity();
+
+  OdometryIt odometry_gt_it = odometry_gt_msgs.begin();
 
   std::size_t scan_id = 0;
   // Pass all scans
-  for (const auto &p : localized_scans)
+  for (const auto ros_scan : laser_scan_msgs)
   {
     if (!ros::ok())
     {
@@ -153,31 +185,26 @@ int main(int argc, char **argv)
       break;
     }
 
-    std::cout << "\n\n ------------------------------------"
-              << " \n\tProcessing scan " << scan_id++
-              << "\n ------------------------------------\n" << std::endl;
-
-//    const sensor_msgs::LaserScan& ros_scan = p.first;
-    const sensor_msgs::LaserScanPtr ros_scan = boost::make_shared<sensor_msgs::LaserScan>(p.first);
+    std::cout << "\n\n------------------------------------"
+              << "\n\tProcessing scan " << scan_id++
+              << "\n------------------------------------\n" << std::endl;
 
     // Get tf with respect to initial pose being I &
     // convert to lsr format.
-    corrected_pose = origin_inv * p.second;
+    corrected_pose = origin_inv /** p.second*/;
 
     // Get current tf with respect to previous one
 //    const tf::Transform corrected_relative_pose = tf_prev.inverse() * corrected_pose;
 
     /* ----------- Everything Takes Place Here ---------- */
 
-    geometry_msgs::PoseWithCovarianceStampedPtr estimated_pose;
-    estimated_pose = boost::make_shared<geometry_msgs::PoseWithCovarianceStamped>();
+    nav_msgs::OdometryPtr estimated_pose;
+    estimated_pose = boost::make_shared<nav_msgs::Odometry>();
 
     /// @todo
     if (use_odom_prior) laser_odom_ptr->setInitialGuess(initial_guess);
 
     /*bool processed =*/ laser_odom_ptr->process(ros_scan, estimated_pose);
-
-    std::cout << "Process done." << std::endl;
 
     //  @todo.process(ros_scan, global_pose, relative_pose);
 
@@ -190,37 +217,30 @@ int main(int argc, char **argv)
     geometry_msgs::Transform corrected_pose_msg;
     tf::transformTFToMsg(corrected_pose, corrected_pose_msg);
 
-    std::cout << "corrected_pose\n"  << corrected_pose_msg << std::endl;
-    std::cout << "estimated_pose\n"  << estimated_pose->pose.pose << std::endl;
-
-//    std::cout << "tf\n"  << tf_w.matrix() << std::endl;
-//    std::cout << "tf_rel "
-//              << " yaw " << extractYaw(tf_rel)
-//              << " t " << tf_rel.translation().norm() << "\n"
-//              << tf_rel.matrix() << std::endl;
-//    std::cout << "tf estimated\n" << tf_est.matrix() << std::endl;
-//    std::cout << "tf_rel estimated "
-//              << " yaw " << extractYaw(tf_rel_est)
-//              << " t " << tf_rel_est.translation().norm() << "\n"
-//              << tf_rel_est.matrix() << std::endl;
+//    std::cout << "corrected_pose : "
+//              << corrected_pose_msg.translation.x << " "
+//              << corrected_pose_msg.translation.y << " "
+//              << tf::getYaw(corrected_pose_msg.rotation) << std::endl;
+    std::cout << "estimated_pose : "
+              << estimated_pose->pose.pose.position.x << " "
+              << estimated_pose->pose.pose.position.y << " "
+              << tf::getYaw(estimated_pose->pose.pose.orientation) << std::endl;
 
 //    tf_prev = corrected_pose;
 
-    //std::cout << "\n\n------Publishing Logs------\n\n" << std::endl;
 
-
+    //std::cout << "\n\n------Publishing------\n\n" << std::endl;
     bool sleep_pub = false;
     if (publisher_corrected_poses.getNumSubscribers()>0)
     {
-      geometry_msgs::Pose corrected_pose_msg2;
-      corrected_pose_msg2.position.x = corrected_pose_msg.translation.x;
-      corrected_pose_msg2.position.y = corrected_pose_msg.translation.y;
-      corrected_pose_msg2.position.z = corrected_pose_msg.translation.z;
-      corrected_pose_msg2.orientation = corrected_pose_msg.rotation;
+      while (odometry_gt_it->header.stamp<ros_scan->header.stamp)
+      {
+        corrected_poses.poses.push_back(odometry_gt_it->pose.pose);
+        corrected_poses.header.frame_id = estimated_pose->header.frame_id;
+        publisher_corrected_poses.publish(corrected_poses);
 
-      corrected_poses.poses.push_back(corrected_pose_msg2);
-      corrected_poses.header.frame_id = estimated_pose->header.frame_id;
-      publisher_corrected_poses.publish(corrected_poses);
+        ++odometry_gt_it;
+      }
 
       sleep_pub = true;
     }
@@ -237,13 +257,9 @@ int main(int argc, char **argv)
     ros::spinOnce();
 
     if (sleep_pub) sleep_duration.sleep();
-
-//    if (scan_id > 70) break;
   }
 
   std::cout << "Took in average : " << timer.avg_tic() << " us." << std::endl;
-
-  std::cout << "Done." << std::endl;
 
   return EXIT_SUCCESS;
 }
